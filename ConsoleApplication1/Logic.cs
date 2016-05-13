fc1eabcbdc8e9a402901962527f2fe8d3667f192
Alex Bihshtein <alexbihsh@gmail.com>
2016-05-13 15:00:17 +0300

    simple speedup and print calcualtion time

--- ConsoleApplication1/Logic.cs
+++ ConsoleApplication1/Logic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,17 +9,24 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1 {
     class Logic {
         public static List<long> GetEquals(List<List<long>> ids,int numOfFiles) {
+            var aleg = new Stopwatch();
+            aleg.Start();
+           // ids.ForEach((list) => list.Sort());
             var equals = new List<long>();
             for (int i = 0; i < ids[0].Count; i++) {
                 var allEquals = true;
-                for (int j = 1; j < numOfFiles; j++) {
-                    if (!ids[j].Contains(ids[0][i]))
-                        allEquals = false;
+                for (int j = 1; j < numOfFiles/* && allEquals*/; j++) {
+             /*       while
+                    if (!ids[j][i] > ids[0][i])
+                        allEquals = false;*/
+                     if (!ids[j].Contains(ids[0][i]))                     
+                         allEquals = false;
                 }
                 if (allEquals)
                     equals.Add(ids[0][i]);
             }
-            Console.WriteLine(string.Format("Matching id's : {0}", equals.Count.ToString()));
+            aleg.Stop();
+            Console.WriteLine(string.Format("Matching id's : {0}, calcualtion time {1} seconds ", equals.Count.ToString(), aleg.Elapsed.TotalSeconds));
             return equals;
         }
 
