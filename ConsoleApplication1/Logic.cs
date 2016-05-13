5f58edd25b670803ee4bb90df4c7c30180ba0ee6
Alex Bihshtein <alexbihsh@gmail.com>
2016-05-13 13:38:53 +0300

    Create logic class, 6 bigest stores is the magic number

--- ConsoleApplication1/Logic.cs
+++ ConsoleApplication1/Logic.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1 {
     class Logic {
+        public static List<long> GetEquals(List<List<long>> ids,int numOfFiles) {
+            var equals = new List<long>();
+            for (int i = 0; i < ids[0].Count; i++) {
+                var allEquals = true;
+                for (int j = 1; j < numOfFiles; j++) {
+                    if (!ids[j].Contains(ids[0][i]))
+                        allEquals = false;
+                }
+                if (allEquals)
+                    equals.Add(ids[0][i]);
+            }
+            Console.WriteLine(string.Format("Matching id's : {0}", equals.Count.ToString()));
+            return equals;
+        }
+
+        public static List<Dictionary<long, ItemData>> GetData(List<FileInfo> files, List<long> equals) {
+            var storesData = new List<Dictionary<long, ItemData>>();
+            foreach (var file in files) {
+                var data = Download.GetData(equals, File.ReadAllText(file.FullName));
+                storesData.Add(data);
+                double total = 0;
+                foreach (var value in data.Values) {
+                    total += value.Price;
+                }
+                Console.WriteLine(Path.GetFileName(file.FullName) + "  :  " + total);
+            }
+            return storesData;
+        }
     }
 }
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -19,9 +19,20 @@ namespace ConsoleApplication1 {
         public enum ArgsOptions {
             download
         }
-      
+           
+        static void Main(string[] args) {
+            if (args.Length > 0)
+                if (args[0] == ArgsOptions.download.ToString())
+                    DownloadAll();
+            var files = new DirectoryInfo(Download.FolderPath).GetFiles().OrderByDescending(x => x.Length).Take(6).ToList<FileInfo>();
+            List<List<long>> ids = new List<List<long>>();
+            files.ForEach((file) => ids.Add(Download.GetIDS(File.ReadAllText(file.FullName), "ItemCode")));
+            var equals = Logic.GetEquals(ids, files.Count);
+            var storesData = Logic.GetData(files, equals);
+        }
+
         public static void DownloadCompanyFullPrices(CompanyEnum company) {
-           Download.GetShopIds(company).ForEach((id) => Download.DownloadFullPriceFile(company, id));
+            Download.GetShopIds(company).ForEach((id) => Download.DownloadFullPriceFile(company, id));
         }
 
         public static void DownloadAll() {
@@ -29,38 +40,6 @@ namespace ConsoleApplication1 {
                 DownloadCompanyFullPrices(company);
             }
         }
-
-        static void Main(string[] args) {
-            if (args.Length > 0)
-                if (args[0] == ArgsOptions.download.ToString())
-                    DownloadAll();
-            List<Dictionary<long, ItemData>> storesData = new List<Dictionary<long, ItemData>>();
-
-            List<string> files = Directory.EnumerateFiles(Download.FolderPath, "*.xml").ToList<string>();
-            List<List<long>> ids = new List<List<long>>();
-            files.ForEach((file) => ids.Add(Download.GetIDS(File.ReadAllText(file), "ItemCode")));
-            List<long> equals = new List<long>();
-            for (int i = 0; i < ids[0].Count; i++) {
-                var allEquals = true;
-                for (int j = 1; j < files.Count; j++) {
-                    if (!ids[j].Contains(ids[0][i]))
-                        allEquals = false;
-                }
-                if (allEquals)
-                    equals.Add(ids[0][i]);
-            }
-            Console.WriteLine(string.Format("Matching id's : {0}", equals.Count.ToString()));
-            foreach (var file  in files) {
-                var data = Download.GetData(equals, File.ReadAllText(file));
-                storesData.Add(data);
-                double total = 0;
-                foreach (var value in data.Values) {
-                    total += value.Price;
-                }
-                Console.WriteLine(Path.GetFileName(file) +"  :  " + total);
-            }
-        }
-      
     }
 }
 
