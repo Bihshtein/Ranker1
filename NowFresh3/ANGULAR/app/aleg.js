3765e99660564852a1a4a09cde9f63121d7ab9ad
Alex Bihshtein <alexbihsh@gmail.com>
2016-08-03 23:21:23 +0300

    first working site version

--- NowFresh3/ANGULAR/app/aleg.js
+++ NowFresh3/ANGULAR/app/aleg.js
@@ -6,9 +6,9 @@
         .controller('Hello', Hello);
 
 function Hello($scope, $http) {
-    $http.get('http://localhost:51612/Api/Students/Apple').
+    $http.get('http://localhost:51612/Api/Students/').
         then(function (data) {
-            $scope.greeting = data;
+            $scope.greeting =  angular.fromJson(data);
         });
 }
 
