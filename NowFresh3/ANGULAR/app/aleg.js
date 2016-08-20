47e42734ef1e6e9f846693715e5631abc36b5e98
Alex Bihshtein <alexbihsh@gmail.com>
2016-08-20 22:10:49 +0300

    query both for main and sec

--- NowFresh3/ANGULAR/app/aleg.js
+++ NowFresh3/ANGULAR/app/aleg.js
@@ -6,26 +6,67 @@
         .controller('Hello', Hello);
 
 function Hello($scope, $http) {
-    $http.get('http://localhost:51612/Api/Students/Citrus').
+    $http.get('http://localhost:51612/Api/Students/Sec=Citrus').
         then(function (data) {
             $scope.Citrus =  angular.fromJson(data);
         });
-
-    $http.get('http://localhost:51612/Api/Students/Tropic').
+    $http.get('http://localhost:51612/Api/Students/Sec=Tropic').
       then(function (data) {
           $scope.Tropic = angular.fromJson(data);
       });
 
-
-    $http.get('http://localhost:51612/Api/Students/Tree').
+    $http.get('http://localhost:51612/Api/Students/Sec=Tree').
       then(function (data) {
           $scope.Tree = angular.fromJson(data);
       });
 
-    $http.get('http://localhost:51612/Api/Students/Bush').
+    $http.get('http://localhost:51612/Api/Students/Sec=Bush').
       then(function (data) {
           $scope.Bush = angular.fromJson(data);
       });
+
+    $http.get('http://localhost:51612/Api/Students/Sec=Nut').
+        then(function (data) {
+            $scope.Nut = angular.fromJson(data);
+        });
+    $http.get('http://localhost:51612/Api/Students/Sec=Root').
+      then(function (data) {
+          $scope.Root = angular.fromJson(data);
+      });
+    $http.get('http://localhost:51612/Api/Students/Sec=Flower').
+      then(function (data) {
+          $scope.Flower = angular.fromJson(data);
+      });
+    $http.get('http://localhost:51612/Api/Students/Sec=Fruit').
+      then(function (data) {
+          $scope.SecFruit = angular.fromJson(data);
+      });
+    $http.get('http://localhost:51612/Api/Students/Sec=Seed').
+       then(function (data) {
+           $scope.Seed = angular.fromJson(data);
+       });
+    $http.get('http://localhost:51612/Api/Students/Sec=Leaf').
+      then(function (data) {
+          $scope.Leaf = angular.fromJson(data);
+      });
+    $http.get('http://localhost:51612/Api/Students/Sec=Tubur').
+      then(function (data) {
+          $scope.Tubur = angular.fromJson(data);
+      });
+    $http.get('http://localhost:51612/Api/Students/Sec=Fungus').
+      then(function (data) {
+          $scope.Fungus = angular.fromJson(data);
+      });
+
+    $http.get('http://localhost:51612/Api/Students/Main=Fruit').
+     then(function (data) {
+         $scope.MainFruit = angular.fromJson(data);
+     });
+    $http.get('http://localhost:51612/Api/Students/Main=Veg').
+      then(function (data) {
+          $scope.Veg = angular.fromJson(data);
+      });
+
 }
 
 })();
