4c587f7666c2da4a98fd0a3f3f0ffd7b957fe5e8
Alex Bihshtein <alexbihsh@gmail.com>
2016-12-07 00:55:19 +0200

    get the meal type

--- EatRight/InitMenus/Program.cs
+++ EatRight/InitMenus/Program.cs
@@ -11,7 +11,7 @@ namespace InitRecipes {
     class Program {
         static void Main(string[] args) {
 
-            for (int i = 220000; i < 300000; ++i) {
+            for (int i = 230000; i < 300000; ++i) {
                 ParseRecipe(i.ToString());
             }
         }
@@ -23,8 +23,13 @@ namespace InitRecipes {
             }
             catch { return; }
             var nameParts = page.Split(new string[2] { "<title>", "</title>" }, StringSplitOptions.None);
+            var mealParts = page.Split(new string[1] { "<span class=\"toggle-similar__title\" itemprop=\"title\">" }, StringSplitOptions.None);
+            if (mealParts.Length != 4)
+                return;
             Console.WriteLine("Num : " + id);
             Console.WriteLine("Title : " + nameParts[1]);
+            var mealType = new String(mealParts[3].TakeWhile(a => a != '<').ToArray()).Trim();
+            Console.WriteLine("Meal type : " + mealType);
             var ingredientParts = page.Split(new string[1] { "itemprop=\"ingredients\">" }, StringSplitOptions.None);
             var ingredients = new List<string>();
             for (int i = 1; i < ingredientParts.Length; i++) { 
