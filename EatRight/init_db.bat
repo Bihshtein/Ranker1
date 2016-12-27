b933bf549a4299db0ded4ecab64570c7fedb8a5b
Alex Bihshtein <alexbihsh@gmail.com>
2016-12-27 20:57:28 +0200

    add commands to start and init db and execute inint after InitDB build

---
+++ EatRight/init_db.bat
@@ -0,0 +1,3 @@
+C:\dev\Ranker\EatRight\InitDB\bin\Debug\InitDB.exe
+C:\dev\Ranker\EatRight\InitDailyValuesDB\bin\Debug\InitDailyValuesDB.exe
+C:\dev\Ranker\EatRight\InitMenus\bin\Debug\InitRecipes.exe
