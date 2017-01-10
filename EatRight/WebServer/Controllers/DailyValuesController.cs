2aa71b207c73c12ab9898583ae64d924fee8673e
Alex Bihshtein <alexbihsh@gmail.com>
2017-01-11 01:28:23 +0200

    add daily values controller

---
+++ EatRight/WebServer/Controllers/DailyValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Web.Http;
+using RestModel;
+using System.Net.Http;
+using System.Net;
+using System.Collections.Generic;
+using System;
+namespace WebServer.Controllers {
+    public class DailyValuesController : ApiController {
+        public HttpResponseMessage GetAll() {
+            var values = new RestDBInterface().DailyValues.GetAll();
+            var x = new List<List<DailyValue>>();
+            x.Add(values.ToList());
+            if (values.Any()) return Request.CreateResponse(HttpStatusCode.OK, x);
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No products found.");
+        }
+    }
+}
