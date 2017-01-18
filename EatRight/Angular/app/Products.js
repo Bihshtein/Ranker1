b6e1da8e148515c19b929acb14eb9437e14d05dc
Alex Bihshtein <alexbihsh@gmail.com>
2017-01-19 01:22:04 +0200

    dinamically change query colors and clean the code

--- EatRight/Angular/app/Products.js
+++ EatRight/Angular/app/Products.js
@@ -11,24 +11,19 @@
     angular
         .module('app')
         .controller('Query', Hello);
-    function Hello2($scope) {
-        $scope.aleg = "aleg";
-    }
+  
     function Hello($scope, $http) {
-        $scope.init = function (query, min, products) {
-            $http.get('http://localhost:51612/Api/Products/' + query + '=' + min + ',' + products).
+        $scope.init = function (query) {
+            $http.get('http://localhost:51612/Api/Products/' + query).
                 then(function (data) {
-                    $scope.dict[query] = angular.fromJson(data);
+                    if ($scope.dict != null)
+                        $scope.dict[query] = angular.fromJson(data);
+                    else 
+                        $scope.dict = {
+                            query : angular.fromJson(data),
+                        };
                 });
         };
-
-        $http.get('http://localhost:51612/Api/Products/').
-        then(function (data) {
-            $scope.dict = {
-                "All": angular.fromJson(data),
-            };
-        });
-
 }
 
 })();
