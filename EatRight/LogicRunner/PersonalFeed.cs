acbf37a2b4962a15bc4dc0fb7daee0534253915f
Alex Bihshtein <alexbihsh@gmail.com>
2017-05-22 00:17:42 +0300

    improve UI

--- EatRight/LogicRunner/PersonalFeed.cs
+++ EatRight/LogicRunner/PersonalFeed.cs
@@ -52,14 +52,14 @@ namespace LogicRunner {
                     (m.GradeInfo.GradersInfo[GraderType.MinNutValuesMealGrader].Grade *3) +
                     m.GradeInfo.GradersInfo[GraderType.CaloriesCountMealGrader].Grade) / 6 * 100);
 
-                var strRecipe = "<div><font style=\"background-color:{0};font-weight: bold;\">{1}</font></div>";
                 var strImage = "<a href=\"{0}\"><img src=\"{1}\"  height=\"300\" width=\"300\"></a>";
                 var format = "<div><font style=\"font-weight: bold;\">{0}</font></div>";
                 var resMax = m.GradeInfo.MaxNutrientGrades.OrderBy(e => e.Value).ToList();
                 var resMin = m.GradeInfo.MinNutrientGrades.OrderByDescending(e => e.Value).ToList().Take(3).ToList();
-                body += string.Format(strRecipe, "Beige", m.Recipe.Name+", Score : " + nutritionScore.ToString());
+                body += string.Format(format,m.Recipe.Name);                
                 body += string.Format(strImage, "http://bit.ly" + link, m.Recipe.ImageUrl);
-                body += string.Format(format,  "Rich with : "+ resMin[0].Key.Split(',')[0] + ", "+ resMin[1].Key.Split(',')[0] + ", " +resMin[2].Key.Split(',')[0]);
+                body += string.Format(format, "Score : " + nutritionScore.ToString());
+                body += string.Format(format,  "Rich with : "+ resMin[0].Key.Split(',')[0] + ", "+ resMin[1].Key.Split(',')[0]);
                 if (resMax[0].Value < 1) {
                     body += string.Format(format, "Too much : " + resMax[0].Key.Split(',')[0]);
                 }
@@ -68,7 +68,7 @@ namespace LogicRunner {
             body += "</tr>";
             body += "</table>";
 
-            body += "<div>For any requests or concerns please reply to this address</div>";
+            body += "<div><p>For any requests or concerns please reply to this address</p></div>";
             body = "<!DOCTYPE html><html><body>" + body + "</html></body>";
             var address = recommendationDB.UserProfile.Email;
             if (debug)
