2b72d2590ef21a2fd6dffa1efeeb68a7466f2e6e
Alex Bihshtein <alexbihsh@gmail.com>
2017-05-23 23:01:13 +0300

    meh

--- EatRight/LocalDB/init_db.bat
+++ EatRight/LocalDB/init_db.bat
@@ -1,5 +1,5 @@
 ..\InitUsers\bin\Debug\InitUsers.exe
 ..\InitProducts\bin\Debug\InitProducts.exe
 ..\InitDailyValues\bin\Debug\InitDailyValues.exe
-..\InitRecipes\bin\Debug\InitRecipes.exe 10000 CLEAR OFFLINE
+..\InitRecipes\bin\Debug\InitRecipes.exe 100000 CLEAR OFFLINE
 
